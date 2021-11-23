ca8904052e1191a80f4fc5e6c2be0266944335c0
Murilo Gois de Almeida <39069727+goismurilo@users.noreply.github.com>
2021-11-23 01:21:56 -0300

    refactor(Auth): Change method authentication

--- lib/modules/login/login_controlller.dart
+++ lib/modules/login/login_controlller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 import 'package:payflow/shared/auth/auth_controller.dart';
+import 'package:payflow/shared/models/user_models.dart';
 
 class LoginController {
   final authController = AuthController();
@@ -13,7 +14,9 @@ class LoginController {
     );
     try {
       final response = await _googleSignIn.signIn();
-      authController.setUser(context, response);
+      final user =
+          UserModel(name: response!.displayName!, photoURL: response.photoUrl);
+      authController.setUser(context, user);
       print(response);
     } catch (error) {
       authController.setUser(context, null);
