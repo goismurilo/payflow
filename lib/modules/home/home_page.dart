cda6aacb3781f54ed8e3ce058c978a5b7d3663a9
Murilo Gois de Almeida <39069727+goismurilo@users.noreply.github.com>
2021-11-23 02:11:24 -0300

    fix(Styles): Change colors

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
 
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("HomePage")),
-    );
+        appBar: PreferredSize(
+      preferredSize: Size.fromHeight(152),
+      child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text("Olá, Murilo", style: TextStyles.titleRegular),
+              subtitle: Text("Mantenha as suas contas em dia",
+                  style: TextStyles.captionShape),
+              trailing: Container(
+                  height: 48,
+                  width: 48,
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(5))),
+            ),
+          )),
+    ));
   }
 }
